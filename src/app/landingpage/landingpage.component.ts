9803de6e2c02fe59f787978801b8ee03f7ef1e10
asreal089 <lexx_89@hotmail.com>
2021-10-21 15:35:39 -0300

    começando trabalhos na landing page, redirect para login e singin

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landingpage',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingpageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  login(){
+    this.router.navigate(['/login']);
+  }
+
+  signin(){
+    this.router.navigate(['/signin']);
+  }
 }
