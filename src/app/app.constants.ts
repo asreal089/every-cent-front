e486b549bc99ee1f13f5f905395529df4540e736
asreal089 <lexx_89@hotmail.com>
2023-04-19 23:42:26 -0300

    Fix constants

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -3,9 +3,9 @@ import { environment } from './../environments/environment';
 
 export class AppConstants {
     private static API_BASE_URL = environment.apiBaseUrl;
-    private static OAUTH2_URL = environment.apiBaseUrl + "oauth2/authorization/";
+    private static OAUTH2_URL = AppConstants.API_BASE_URL + "oauth2/authorization/";
     private static REDIRECT_URL = environment.redirectUrl;
-    public static API_URL = environment.apiBaseUrl + "api/";
+    public static API_URL = AppConstants.API_BASE_URL + "api/";
     public static AUTH_API = AppConstants.API_URL + "auth/";
     public static GOOGLE_AUTH_URL = AppConstants.OAUTH2_URL + "google" + AppConstants.REDIRECT_URL;
     public static TIPO_LANCAMENTO_URL = AppConstants.API_URL+"tipo-lancamento";
